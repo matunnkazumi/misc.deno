321763bd41e5f8352ffdb06de6d1ae0c3981ef9f
matunnkazumi <developer@matunnkazumi.space>
2022-02-06 17:01:49 +0900

    フォーマット

--- pdf_unite.ts
+++ pdf_unite.ts
@@ -1,7 +1,7 @@
 import {
   PDFDocument,
-  ReadingDirection,
   PDFName,
+  ReadingDirection,
 } from "https://cdn.skypack.dev/pdf-lib@^1.11.1?dts";
 
 function is_pdf_file(entry: Deno.DirEntry) {
@@ -14,30 +14,30 @@ function is_pdf_file(entry: Deno.DirEntry) {
   return true;
 }
 const result = Array.from(Deno.readDirSync("./"))
-                .filter(is_pdf_file)
-                .map(e => e.name)
-                .sort()
-                .map(filename => Deno.readFileSync(filename))
-                .map(bytes => PDFDocument.load(bytes));
+  .filter(is_pdf_file)
+  .map((e) => e.name)
+  .sort()
+  .map((filename) => Deno.readFileSync(filename))
+  .map((bytes) => PDFDocument.load(bytes));
 const pdfDocs = await Promise.all(result);
 
 const first = pdfDocs[0];
 
-const viewerPrefs = first.catalog.getOrCreateViewerPreferences()
+const viewerPrefs = first.catalog.getOrCreateViewerPreferences();
 viewerPrefs.setReadingDirection(ReadingDirection.R2L);
 
-first.catalog.set(PDFName.of('PageLayout'), PDFName.of('TwoColumnRight'))
+first.catalog.set(PDFName.of("PageLayout"), PDFName.of("TwoColumnRight"));
 
-const pagesPromise = pdfDocs.slice(1).map(doc => {
+const pagesPromise = pdfDocs.slice(1).map((doc) => {
   const count = doc.getPageCount();
-  const indices = Array.from({length: count}, (_, i) => i);
-  return first.copyPages(doc, indices)
-})
+  const indices = Array.from({ length: count }, (_, i) => i);
+  return first.copyPages(doc, indices);
+});
 const copied = await Promise.all(pagesPromise);
 
-copied.forEach(pages => {
-  pages.forEach(page => first.addPage(page));
-})
+copied.forEach((pages) => {
+  pages.forEach((page) => first.addPage(page));
+});
 
 const newCount = first.getPageCount();
 const pdfBytes = await first.save();
