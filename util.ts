f5efb15090c55db937d00a4a7d4718edbb71b45e
matunnkazumi <developer@matunnkazumi.space>
2022-03-20 20:55:15 +0900

    ローカルに一時フォルダと一時ファイルを作る

--- util.ts
+++ util.ts
@@ -4,3 +4,29 @@ export function date_now_jst_format(): string {
   const jst_date = datetime().toZonedTime("Asia/Tokyo");
   return jst_date.format(`YYYYMMdd`);
 }
+
+import { ensureDir } from "https://deno.land/std@0.130.0/fs/mod.ts";
+
+export type MakeTempDirOptions = {
+  prefix: string;
+};
+
+export async function makeTempDir(option?: MakeTempDirOptions) {
+  const random = crypto.randomUUID();
+  const dirName = (option?.prefix ?? "") + random;
+  await ensureDir(dirName);
+  return dirName;
+}
+
+export type MakeTempFileOptions = {
+  prefix: string;
+};
+
+export async function makeTempFile(option?: MakeTempDirOptions) {
+  const random = crypto.randomUUID();
+  const fileName = (option?.prefix ?? "") + random;
+
+  await Deno.create(fileName);
+
+  return fileName;
+}
