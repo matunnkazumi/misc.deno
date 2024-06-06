5d105140f38eea0ca362f3acd0f577d2e00d225a
matunnkazumi <developer@matunnkazumi.space>
2024-06-07 08:34:05 +0900

    h265 VAAPI エンコード

--- avi_convert.ts
+++ avi_convert.ts
@@ -16,8 +16,9 @@ export interface ConvertOption {
     left: number;
     top: number;
   };
-  vcodec: "lossless" | "h265" | "av1";
+  vcodec: "lossless" | "h265" | "h265_vaapi" | "av1";
   concurrentLimit?: number;
+  device?: string;
 }
 
 async function avi_recompless(
@@ -49,6 +50,10 @@ async function avi_recompless(
       } else if (vcodec == "h265") {
         // https://life.craftz.dog/entry/save-storage-with-h265-ffmpeg
         await $`ffmpeg -i ${file.srcFileName} ${toOpt} ${toParam} -vf ${filters} -c:v libx265 -crf 18 -b:v 0 -tag:v hvc1 -c:a aac -b:a 320k ${file.newFileName}`;
+      } else if (vcodec == "h265_vaapi") {
+        const device = param.device;
+        const vf = `${filters},format=nv12,hwupload`;
+        await $`ffmpeg -vaapi_device ${device} -i ${file.srcFileName} ${toOpt} ${toParam} -vf ${vf} -c:v hevc_vaapi -qp 23 -rc_mode CQP -tag:v hvc1 -c:a aac -b:a 320k ${file.newFileName}`;
       } else if (vcodec == "av1") {
         // https://gitlab.com/AOMediaCodec/SVT-AV1/-/blob/master/Docs/Ffmpeg.md
         await $`ffmpeg -i ${file.srcFileName} ${toOpt} ${toParam} -vf ${filters} -c:v libsvtav1 -preset 5 -crf 32 -g 240 -pix_fmt yuv420p10le -svtav1-params tune=0 -c:a libopus -b:a 320k ${file.newFileName}`;
