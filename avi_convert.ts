7b2958b3adb4e836541615bdf42986d28b4d4c1a
matunnkazumi <developer@matunnkazumi.space>
2024-06-07 08:09:23 +0900

    change AV1 reference

--- avi_convert.ts
+++ avi_convert.ts
@@ -52,8 +52,8 @@ async function avi_recompless(
         // https://life.craftz.dog/entry/save-storage-with-h265-ffmpeg
         await $`ffmpeg -i ${file.srcFileName} ${toOpt} ${toParam} -vf ${filters} -c:v libx265 -crf 18 -b:v 0 -tag:v hvc1 -c:a aac -b:a 320k ${file.newFileName}`;
       } else if (vcodec == "av1") {
-        // https://www.osumiakari.jp/articles/20231116-ffmpeg-svtav1/
-        await $`ffmpeg -i ${file.srcFileName} ${toOpt} ${toParam} -vf ${filters} -c:v libsvtav1 -preset 5 -svtav1-params tune=0:tile_columns=2:tile_rows=2 -c:a libopus -b:a 320k ${file.newFileName}`;
+        // https://gitlab.com/AOMediaCodec/SVT-AV1/-/blob/master/Docs/Ffmpeg.md
+        await $`ffmpeg -i ${file.srcFileName} ${toOpt} ${toParam} -vf ${filters} -c:v libsvtav1 -preset 5 -crf 32 -g 240 -pix_fmt yuv420p10le -svtav1-params tune=0 -c:a libopus -b:a 320k ${file.newFileName}`;
       }
     },
   );
