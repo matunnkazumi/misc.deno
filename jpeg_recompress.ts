ec87200ff502ac55d03dc9bdb9b3214870a76d4b
matunnkazumi <developer@matunnkazumi.space>
2026-01-24 20:35:46 +0900

    jpeg の orientation 対応

--- jpeg_recompress.ts
+++ jpeg_recompress.ts
@@ -28,7 +28,7 @@ export async function jpeg_recompless(
   }).map(async (file) => {
     const temp_file_name = await makeTempFile({ prefix: temp_dir + "/" });
 
-    await $`convert -resize ${param.resize_width}x -quality 100 -unsharp 0x0.75+0.75+0.008 ${file.srcFileName} ${temp_file_name}`;
+    await $`convert -auto-orient -resize ${param.resize_width}x -quality 100 -unsharp 0x0.75+0.75+0.008 ${file.srcFileName} ${temp_file_name}`;
     await $`guetzli --quality 84 ${temp_file_name} ${file.newFileName}`;
     await $`exiftool -overwrite_original -UserComment="https://matunnkazumi.blog.fc2.com/" ${file.newFileName}`;
   });
